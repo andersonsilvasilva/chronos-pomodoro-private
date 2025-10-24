fbecc9bb004e3701673ffa367ce937e5614918f1
Anderson Barros <barrosanderson@gmail.com>
2025-10-24 16:24:13 -0300

    fim da 1aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   console.log('App component rendered');
   return (
-    <div>
+    <>
       <h1>Olá Mundo</h1>
-    </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sunt,
+        fugiat repudiandae dolore velit facilis iste quam deleniti pariatur
+        minima ab voluptatum beatae nulla quaerat minus totam ut. Minus, culpa.
+      </p>
+    </>
   );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import QlqCoisa from './App';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>
-      <h1>Olá Mundo</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sunt,
-        fugiat repudiandae dolore velit facilis iste quam deleniti pariatur
-        minima ab voluptatum beatae nulla quaerat minus totam ut. Minus, culpa.
-      </p>
-    </div>
+    <QlqCoisa />
   </StrictMode>,
 );
